e5bee935ff8a221c4aacf209b4d4b4df597cbbbd
Aitor Pascual <apascual053@ikasle.ehu.eus>
2025-02-16 22:28:23 +0100

    Ordinary commit

--- Doc/source/conf.py
+++ Doc/source/conf.py
@@ -31,6 +31,9 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_copybutton',
+    'sphinx_prompt'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,8 +58,3 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'furo'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
